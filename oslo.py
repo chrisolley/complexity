b74ccd1c664b3ce1b90ee8bd11d81eff86403399
chrisolley <olley.cj@gmail.com>
2018-01-18 18:33:49 +0000

    first commit

--- oslo.py
+++ oslo.py
@@ -3,8 +3,8 @@ import numpy as np
 import matplotlib.pyplot as plt
 import random 
 
-L = 100 # system size
-N = 10000 # number of iterations
+L = 10 # number of lattice sites
+N = 50 # number of iterations
 z = np.zeros(L)
 
 
@@ -28,53 +28,68 @@ def thr_init(p):
 
 # initialisation of threshold values
 zth = [thr_init(0.5) for i in range(L)]
-    
+        
 print(zth)
 print(z)
 
+# initialise array to hold size of avalanches
 avalanche_sizes = []
+
+# run loop for the specified number of iterations
+#TODO run until steady state reached
+#TODO measure heights
 
 for i in range(N): 
     
     # drive phase
     z[0] += 1
-
+    print("zth: ", zth)
+    print("z before relaxation: ", z)
     # relaxation phase
-    s_list = []
+    # initialise array to hold value of sites that have relaxed
+    relaxed_sites = []
+    
+    # check all sites to see if gradient above threshold
     for j in range(L):
         
+        # check first site
         if j == 0:
             if z[0] > zth[0]:
                 z[0] -= 2
                 z[1] += 1
-                s_list.append(j)
+                relaxed_sites.append(j)
             
-        elif j == L - 1:
+        # check last site
+        if j == L - 1:
             if z[L - 1] > zth[L - 1]: 
                 z[L - 1] -= 1
                 z[L - 2] += 1
-                s_list.append(j) 
+                relaxed_sites.append(j) 
             
-        else: 
+        # check all other sites
+        if j != 0 and j != L - 1: 
             if z[j] > zth[j]:
                 z[j] -= 2
                 z[j - 1] += 1
                 z[j + 1] += 1
-                s_list.append(j)
+                relaxed_sites.append(j)
     
-    for a in s_list:    
+    # assign new threshold value for every site that relaxed this iteration
+    for a in relaxed_sites:
         zth[a] = thr_init(0.5)
     
-    print("zth: ", zth)
-    print("slist: ", s_list)
-    print("z: ", z)
     
-    avalanche_sizes.append(len(s_list))
+    #print("slist: ", relaxed_sites)
+    print("z after relaxation: ", z)
+    # record size of avalanche that occured on this iteration
+    avalanche_sizes.append(len(relaxed_sites))
     
     #print(s_list)
-print("Record of avalanche sizes: ", avalanche_sizes)
+    
+#print("Record of avalanche sizes: ", avalanche_sizes)
 
 fig, ax = plt.subplots()
 ax.hist(avalanche_sizes, bins = int(np.sqrt(len(avalanche_sizes))), edgecolor = 'black')
+        
 
 plt.show()
